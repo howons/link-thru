16a825e6b3a5f09d319afef3aa0d77d7fa1a17c6
howon <shinhw371@naver.com>
2025-04-03 00:47:10 +0900

    chore: 미사용 설정 제거 및 설명 수정

--- src/popup/components/PopupHeader/PopupHeader.tsx
+++ src/popup/components/PopupHeader/PopupHeader.tsx
@@ -6,11 +6,11 @@ import { Toolbar } from '@mui/material';
 import './PopupHeader.css';
 
 export default function PopupHeader({ children }: { children?: ReactNode }): ReactElement {
-    return (
-        <Toolbar className="popup-header" sx={{ boxShadow: 1 }}>
-            <ExtensionRoundedIcon className="popup-logo" />
-            <h1>Request thru</h1>
-            {children}
-        </Toolbar>
-    );
+  return (
+    <Toolbar className="popup-header" sx={{ boxShadow: 1 }}>
+      <ExtensionRoundedIcon className="popup-logo" />
+      <h1>Link thru</h1>
+      {children}
+    </Toolbar>
+  );
 }
